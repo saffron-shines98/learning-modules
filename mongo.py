7f502f1170b9e9b15cc67309f0f20850692ce481
saffron-shines98 <131915457+saffron-shines98@users.noreply.github.com>
2023-07-22 16:28:12 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,3 +1,5 @@
 import pymongo
 client=pymongo.MongoClient("mongodb+srv://test:test@cluster0.orthvww.mongodb.net/?retryWrites=true&w=majority")
 print("connection established",client)
+database_name="Craftsvilla"
+collection_name= "Search-log"
